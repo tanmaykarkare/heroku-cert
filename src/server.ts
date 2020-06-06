37f11eea0bc2da6d55c2048c764e949c27a6ce84
Tanmay Karkare <tanmay@cloudsmart.co.uk>
2020-06-06 21:14:01 +0100

    version2

--- src/server.ts
+++ src/server.ts
@@ -5,6 +5,6 @@ const PORT = process.env.PORT || 3000;
 
 const app = express();
 app.use(cors());
-app.get('/', (req, res) => res.send({ hello: 'world' }));
+app.get('/', (req, res) => res.send({ hello: 'world 2' }));
 // eslint-disable-next-line
 app.listen(PORT, () => console.log(`Example app listening on port ${PORT}!`));
